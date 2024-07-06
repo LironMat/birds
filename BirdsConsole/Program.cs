a3bd1821c8619a3cfe48fa0e5d99b96bdb01e0da
LironMat <lironi1111@gmail.com>
2024-07-06 13:08:32 +0300

    initial

---
+++ BirdsConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿using BirdsLibrary;
+
+var BRANCH_COUNT = 14;
+var BRANCH_SIZE = 5;
+var BIRD_TYPE_COUNT = 10;
+var BIRD_COUNT = 50;
+
+var birdGame = new BirdsGame(BRANCH_COUNT, BRANCH_SIZE, BIRD_TYPE_COUNT, BIRD_COUNT);
+
+while (true)
+{
+    PrintBoard(birdGame);
+
+
+    while (true)
+    {
+        Console.WriteLine("Enter [fromBranch] [toBranch]");
+
+        var input = Console.ReadLine() ?? "";
+        var splitted = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        if (splitted.Length != 2)
+        {
+            continue;
+        }
+
+        if (!int.TryParse(splitted[0], out var fromBranch) || !int.TryParse(splitted[1], out var toBranch))
+        {
+            continue;
+        }
+
+        if (birdGame.MoveBranches(fromBranch, toBranch))
+        {
+            break;
+        }
+    }
+
+    if (birdGame.GameWon())
+    {
+        break;
+    }
+}
+
+
+string BranchToString<T>(IList<T> branches, int branchIndex) where T : IList<int?>
+{
+    var birdChars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    var branch = branches[branchIndex];
+    var birdsString = string.Join("", branch.Where(b => b is not null).Select(b => birdChars[b.Value]));
+
+    return $"[{branchIndex}]:[{birdsString}]";
+}
+
+void PrintBoard(BirdsGame birdGame)
+{
+    Console.Clear();
+
+    var _branches = birdGame.Branches;
+
+    for (int i = 0; i < _branches.Count; i += 2)
+    {
+        Console.Write(BranchToString(_branches, i));
+
+        if (i + 1 < _branches.Count)
+        {
+            Console.Write($" {BranchToString(_branches, i + 1)}");
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
---
+++ BirdsLibrary/BirdsGame.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BirdsLibrary;
+
+public class BirdsGame
+{
+    public readonly Random _random = new();
+
+    public int BranchCount { get; }
+    public int BranchSize { get; }
+
+    private int?[][] _branches;
+    public ReadOnlyCollection<ReadOnlyCollection<int?>> Branches =>
+        _branches.Select(b => b.AsReadOnly()).ToList().AsReadOnly();
+
+    public BirdsGame(int branchCount, int branchSize, int birdTypeCount, int birdCount)
+    {
+        BranchCount = branchCount;
+        BranchSize = branchSize;
+
+        _branches = new int?[branchCount][];
+
+        for (int branchIndex = 0; branchIndex < branchCount; branchIndex++)
+        {
+            _branches[branchIndex] = new int?[branchSize];
+        }
+
+        var birds = Enumerable.Range(0, birdTypeCount)
+                              .Select(t => Enumerable.Repeat(t, birdCount / birdTypeCount))
+                              .SelectMany(ba => ba)
+                              .OrderBy(b => _random.Next())
+                              .ToList();
+
+        for (int birdIndex = 0; birdIndex < birds.Count; birdIndex++)
+        {
+            var randomBranch = _branches[_random.Next(BranchCount)];
+            var firstFreeSpace = Array.FindIndex(randomBranch, b => b is null);
+
+            if (firstFreeSpace == -1)
+            {
+                birdIndex--;
+            }
+            else
+            {
+                randomBranch[firstFreeSpace] = birds[birdIndex];
+            }
+        }
+    }
+
+    public bool BranchOutOfBounds(int branchIndex)
+    {
+        return branchIndex < 0 || branchIndex >= _branches.Length;
+    }
+
+    public bool BranchIsEmpty(int branchIndex)
+    {
+        return _branches[branchIndex].All(b => b is null);
+    }
+
+    public bool MoveBranches(int fromBranch, int toBranch)
+    {
+        if (BranchOutOfBounds(fromBranch) || BranchOutOfBounds(toBranch))
+        {
+            return false;
+        }
+
+        if (BranchIsEmpty(fromBranch))
+        {
+            return false;
+        }
+
+        var lastBirdFrom = GetLastBird(fromBranch);
+        var lastBirdTo = GetLastBird(toBranch);
+
+        if (lastBirdFrom != lastBirdTo && lastBirdTo is not null)
+        {
+            return false;
+        }
+
+        var takeFromCount = GetSameOnBranchFromEnd(fromBranch, lastBirdFrom);
+        var emptyToCount = GetEmptyCountOnBranch(toBranch);
+
+        if (emptyToCount == 0)
+        {
+            return false;
+        }
+
+        var actualTakeCount = Math.Min(takeFromCount, emptyToCount);
+        var fromBirdCount = Array.FindLastIndex(_branches[fromBranch], b => b is not null);
+        var toFirstFree = Array.FindIndex(_branches[toBranch], b => b is null);
+
+        for (int takeBird = 0; takeBird < actualTakeCount; takeBird++)
+        {
+            _branches[fromBranch][fromBirdCount - takeBird] = null;
+            _branches[toBranch][toFirstFree + takeBird] = lastBirdFrom;
+        }
+
+        if (_branches[toBranch].All(b => b == lastBirdFrom))
+        {
+            _branches[toBranch] = new int?[BranchSize];
+        }
+
+        return true;
+    }
+
+    public int? GetLastBird(int branchIndex)
+    {
+        return _branches[branchIndex].LastOrDefault(b => b is not null);
+    }
+
+    public int GetEmptyCountOnBranch(int branchIndex)
+    {
+        return _branches[branchIndex].Reverse().TakeWhile(b => b is null).Count();
+    }
+
+    public int GetSameOnBranchFromEnd(int branchIndex, int? birdType)
+    {
+        return _branches[branchIndex].Reverse().SkipWhile(b => b is null).TakeWhile(b => b == birdType).Count();
+    }
+
+    public bool GameWon()
+    {
+        return _branches.All(b => b.All(bi => bi is null));
+    }
+}
